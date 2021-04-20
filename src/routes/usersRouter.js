10efdd8b2d0ee65e801b58b2477232f4670eeec6
SylphidZoul <contact@jcruzcardozo.com>
2021-04-20 12:06:42 -0300

    Basic Users route added

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,12 +1,13 @@
 const router = require('express').Router()
 const { controllerTryCatch } = require('../utils')
+const { usersController } = require('../controllers')
 
 router.get(
   '/',
   controllerTryCatch({
     sucessStatusCode: 200,
     errorStatusCode: 500,
-    controller: () => 'Hello world'
+    controller: usersController.getAllUsers
   })
 )
 
