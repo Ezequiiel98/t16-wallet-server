9c6ba14c21b0ed08dd9b326bcc8933fd3173f914
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2021-04-21 19:41:38 -0300

    custom error to handle errors added

--- src/utils/controllerWithTryCatch.js
+++ src/utils/controllerWithTryCatch.js
@@ -1,13 +1,18 @@
 const Response = require('./Response');
 
-const controllerWithTryCatch = ({ controller, sucessStatusCode, errorStatusCode }) => {
+const controllerWithTryCatch = ({ controller, sucessStatusCode }) => {
   return async (req, res, ...restParams) => {
     try {
       const data = await controller(req, res, ...restParams);
 
       Response.success(res, data, sucessStatusCode);
     } catch (error) {
-      Response.error(res, error, error.message, errorStatusCode);
+      Response.error(
+        res, 
+        error,
+        error.message,
+        error.status,
+      );
     }
   }
 };
