75be3f03f25a918c67237ca9cd82a0cf21969129
Hazel Gallegos <hazelgallegos.desarrollo@gmail.com>
2021-04-26 13:57:00 -0500

    Added model, migration and seeder for Transaction. Added relation to Account model.

--- src/db/models/account.js
+++ src/db/models/account.js
@@ -10,7 +10,11 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Account.hasMany(models.Transaction, {
+        foreignKey: 'accountId',
+        as: 'transactions',
+        onDelete: 'CASCADE'
+      })
     }
   };
   Account.init({
