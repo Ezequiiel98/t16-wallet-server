0c55b9b87bbe633bc196e65c273b855a247447c3
Hazel Gallegos <hazelgallegos.desarrollo@gmail.com>
2021-04-26 14:36:58 -0500

    Added correct status code

--- src/controllers/transactionsController.js
+++ src/controllers/transactionsController.js
@@ -4,7 +4,7 @@ const CustomError = require("../utils/CustomError");
 const getAllTransactions = async () => {
   const transactionsFromDB = await db.Transaction.findAll();
 
-  if (!transactionsFromDB) throw new CustomError("No se encontró ningún registro")
+  if (!transactionsFromDB) throw new CustomError({ status: 400, message: 'No se encontró ningún registro' })
 
   return { transactions: transactionsFromDB };
 };
